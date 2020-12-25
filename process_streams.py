08c4792933835070c7104c286f356cb9666dc0a8
Manilal <manihereatcusat@gmail.com>
2020-12-25 22:38:29 +0530

    Text, images and videos link can be filtered out

--- process_streams.py
+++ process_streams.py
@@ -3,51 +3,85 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 import json
 import os
-
-sc = SparkContext("local[2]", "Twitter")
-ssc = StreamingContext(sc,3)
-
-output_dir = 'output/'
-text_file = 'text_file'
-image_file = 'images'
+import matplotlib.pyplot as plt
 
 def process_data(rdd):
-    print('-------------------data is processing---------------')
+    print('-------------------Result of processed data-----------------------')
     l = rdd.collect()           #list containg string
-    
     for s in l:
         d = json.loads(s)       #string to dict
         print(d['id'])
-        
         write_data(d['text'], text_file)
-
-        # if 'entities' in d.keys():
-        #     write_data(json.dumps(d['entities']), 'entities')
-
+ 
         #for image
         if 'entities' in d.keys():
             if 'media' in d['entities']:
                 image_url = d['entities']['media'][0]['media_url']
-                print(image_url)
                 write_data(image_url, image_file)
-                 
+
+                #for video
+                er = d['entities']['media'][0]['expanded_url']
+                if 'video' in er:
+                  write_data(er, video_file)
+    tc = 0
+    ic = 0
+    vc = 0
+    stf = 0
+    sif = 0
+    svf = 0
+    with open(output_dir+text_file) as f:
+      tc = f.read().split(separator)
+      f.seek(0, os.SEEK_END)
+      stf = f.tell()
+    with open(output_dir+image_file) as f:
+      ic = f.read().split(separator)
+      f.seek(0, os.SEEK_END)
+      sif = f.tell()
+    with open(output_dir+video_file) as f:
+      vc = f.read().split(separator)
+      f.seek(0, os.SEEK_END)
+      svf = f.tell()
+
+    print("Number of tweets (text) : " + str(len(tc)-1))
+    print("Number of images : " + str(len(ic)-1))
+    print("Number of video : " + str(len(vc)-1))
+    print("Text file size :" +str(stf) + " Bytes")
+    print("Image file size :" +str(sif) + " Bytes")
+    print("Video file size :" +str(svf) + " Bytes")
+
+    #plotting the data
+    # names = ['text', 'images', video]      #matplotlib is not thread safe
+    # count = [tc,ic,vc]
+    # plt.bar(names, count)
+    # plt.show()
 
 def write_data(data,file_name):
     if not os.path.isdir(output_dir):
       os.mkdir(output_dir)
     with open(output_dir+file_name, 'a') as f:
-      f.write(data+"\n\n")
+      f.write(data+separator)
     print('data witten')
 
 
-#tracking changes in this directory
-data_dir = 'raw_data_3/'
+if __name__ == '__main__':
+  
+  sc = SparkContext("local[2]", "Twitter")
+  ssc = StreamingContext(sc,5)
 
-ds = ssc.textFileStream(data_dir)
-ds.foreachRDD(process_data)
+  output_dir = 'output/'
+  text_file = 'text_file'
+  image_file = 'images'
+  video_file = 'video'
+  expanded_url_file = 'expanded_url'
+  separator = '\n\n'
+
+  #tracking changes in this directory
+  data_dir = 'raw_data_5/'
+
+  ds = ssc.textFileStream(data_dir)
+  ds.foreachRDD(process_data)
 
 
-
-ssc.start()             # Start the computation
-ssc.awaitTermination()  # Wait for the computation to terminate
+  ssc.start()             # Start the computation
+  ssc.awaitTermination()  # Wait for the computation to terminate
 
--- streams.py
+++ streams.py
@@ -1,31 +1,26 @@
-# -*- coding: utf-8 -*-
 
 #for importing twitter_cred
 import sys
 sys.path.append('/home/mani/Desktop/sai/')
 #create this file to hode all the neccessary keys from twiier 
 import twitter_cred
-
 import tweepy
 import socket
 import os
 import json
 import time
 
-
-auth = tweepy.OAuthHandler(twitter_cred.api_key, twitter_cred.api_key_secret)
-auth.set_access_token(twitter_cred.access_token, twitter_cred.access_token_secret)
-api = tweepy.API(auth)
-
 class MyStreamListener(tweepy.StreamListener):
  
+  #callback function
   def on_status(self, data):
     data = data._json   #dict
     print(data['id'])
     self.add_to_file(data)
 
+  
   def add_to_file(self,tweet):
-    data_dir = 'raw_data_3/'
+    data_dir = 'raw_data_5/'
     file_name = tweet['id']
     if not os.path.isdir(data_dir):
       os.mkdir(data_dir)
@@ -40,6 +35,14 @@ class MyStreamListener(tweepy.StreamListener):
       return False
     return True
 
-myStreamListener =  MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener, tweet_mode="extended_tweet")
-myStream.filter(track=['covid'], languages=['en'])
+
+if __name__ == '__main__':
+
+  #authentication
+  auth = tweepy.OAuthHandler(twitter_cred.api_key, twitter_cred.api_key_secret)
+  auth.set_access_token(twitter_cred.access_token, twitter_cred.access_token_secret)
+  api = tweepy.API(auth)
+
+  myStreamListener =  MyStreamListener()
+  myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener, tweet_mode="extended")
+  myStream.filter(track=['india'], languages=['en'])
